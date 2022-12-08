46b0b8c80bcec5599b16c0b185e197cffa8c0721
curtisrosenvall <70542777+curtisrosenvall@users.noreply.github.com>
2022-12-08 14:54:13 -0700

    Creative Project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer class="py-5 bg-dark fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy  <a href="https://github.com/curtisrosenvall">Curt's GitHub</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Home page</h1>
+            <p>
+            <h3>The following is the first few paragraphs of the Star Wars Empire Strikes Back Script!</h3>
+             EXT. GALAXY - PLANET HOTH
+
+A Star Destroyer moves through space, releasing Imperial probe robots 
+from its underside.  
+
+One of these probes zooms toward the planet Hoth and lands on its ice-
+covered surface.  An explosion marks the point of
+impact.  
+
+EXT. HOTH - METEORITE CRATER - SNOW PLAIN - DAY
+
+A weird mechanical sound rises above the whining of the wind.  A 
+strange probe robot, with several extended sensors, emerges from the 
+smoke-shrouded crater.  The ominous mechanical probe floats across the 
+snow plain and disappears into the distance.
+
+EXT. PLAIN OF HOTH - DAY
+
+A small figure gallops across the windswept ice slope.  The bundled 
+rider is mounted on a large gray snow lizard, a Tauntaun.  Curving 
+plumes of snow rise from beneath the speeding paws of the two-legged 
+beast.
+
+The rider gallops up a slope and reins his lizard to a stop.  Pulling 
+off his protective goggles, Luke Skywalker notices something in the 
+sky.  He takes a pair of electrobinoculars from his utility belt and 
+through them sees smoke rising from where the probe robot has crashed.
+
+The wind whips at Luke's fur-lined cap and he activates a comlink 
+transmitter.  His Tauntaun shifts and moans nervously beneath him.
+
+				LUKE
+			(into comlink)
+		Echo Three to Echo Seven.  Han, 
+		old buddy, do you read me?
+
+After a little static a familiar voice is heard.
+
+				HAN
+			(over comlink)
+		Loud and clear, kid.  What's up?
+
+				LUKE
+			(into comlink)
+		Well, I finished my circle.  I 
+		don't pick up any life readings.
+
+				HAN
+			(over comlink)
+		There isn't enough life on this 
+		ice cube to fill a space cruiser.  
+		The sensors are placed, I'm going 
+		back.
+
+				LUKE
+			(into comlink)
+		Right.  I'll see you shortly.  
+		There's a meteorite that hit the 
+		ground near here.  I want to check 
+		it out.  It won't take long.
+
+Luke clicks off his transmitter and reins back on his nervous lizard.  
+He pats the beast on the head to calm it.
+
+				LUKE
+		Hey, steady girl.  What's the 
+		matter?  You smell something?
+
+Luke takes a small device from his belt and starts to adjust it when 
+suddenly a large shadow falls over him from behind.  He hears a 
+monstrous howl and turns to see an eleven-foot-tall shape towering over 
+him.  It is a Wampa Ice Creature, lunging at him ferociously.
+
+				LUKE
+		Aaargh!
+
+Luke grabs for his pistol, but is hit flat in the face by a huge white 
+claw.  He falls unconscious into the snow and in a moment the terrified 
+screams of the Tauntaun are cut short by the horrible snap of a neck 
+being broken.
+
+The Wampa Ice Creature grabs Luke by one ankle and drags him away 
+across the frozen plain.
+
+EXT. HOTH - REBEL BASE ENTRANCE - DAY
+
+A stalwart figure rides his Tauntaun up to the entrance of an enormous 
+ice cave.
+
+INT. HOTH - REBEL BASE - MAIN HANGAR DECK
+
+Rebel troopers rush about unloading supplies and otherwise securing 
+their new base.  The rider, Han Solo, swings off his lizard and pulls 
+off his goggles.
+
+He walks into the main hangar deck toward the Millennium Falcon, which 
+is parked among several fighters.  Mechanics, R2 units, and various 
+other droids hurry about.  Han stops at the Millennium Falcon where his 
+Wookiee copilot, Chewbacca, is welding on a central lifter.  Chewie 
+stops his work and lifts his face shield, growling an irritated 
+greeting to his boss.
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
